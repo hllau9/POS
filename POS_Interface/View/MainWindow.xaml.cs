19d4aeff345164aa7d0ee4180cc80da44330530c
HuiLeong Lau <huileong.lau@aveva.com>
2016-01-30 03:27:40 +0800

    First Commit of Everything

---
+++ POS_Interface/View/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace POS_Interface.View
+{
+    /// <summary>
+    /// Description for MainWindow.
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
