124a3f866c5b4361ba81d6f2d40817ccc06048d6
HuiLeong Lau <huileong.lau@aveva.com>
2016-01-30 16:36:42 +0800

    added a new view

---
+++ POS_Interface/View/view1.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+
+namespace POS_Interface.View
+{
+    /// <summary>
+    /// Description for view1.
+    /// </summary>
+    public partial class view1 : MetroWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the view1 class.
+        /// </summary>
+        public view1()
+        {
+            InitializeComponent();
+        }
+    }
+}
